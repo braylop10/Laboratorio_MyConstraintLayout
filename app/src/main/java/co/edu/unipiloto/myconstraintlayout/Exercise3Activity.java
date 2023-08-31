85cf0fa58c841116b048b9340980f5808271b569
braylop10 <brayanmns10@gmail.com>
2023-08-31 01:40:40 -0500

    Initial commit

---
+++ app/src/main/java/co/edu/unipiloto/myconstraintlayout/Exercise3Activity.java
@@ -0,0 +1,14 @@
+package co.edu.unipiloto.myconstraintlayout;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Exercise3Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_exercise3);
+    }
+}
---
+++ app/src/main/java/co/edu/unipiloto/myconstraintlayout/Exercise4Activity.java
@@ -0,0 +1,14 @@
+package co.edu.unipiloto.myconstraintlayout;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Exercise4Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_exercise4);
+    }
+}
